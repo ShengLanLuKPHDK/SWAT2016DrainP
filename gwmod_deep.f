036233a84b62801249400abd1c319561f971888e
ShengLanLuKPHDK <lvxinglan@gmail.com>
2016-12-06 21:33:54 +0100

    Add files via upload

--- gwmod_deep.f
+++ gwmod_deep.f
@@ -47,11 +47,13 @@
       rchrg1 = 0.
       rchrg1 = gwseep
 
-
+      gwseep1 = gwseep*gw_lost
+      gw_qdeep(j) = gw_qdeep(j) * alpha_bfe_d(j) + (gwseep- gwseep1)*  
+     &                                         (1. - alpha_bfe_d(j))
 !! compute groundwater contribution to streamflow for day (deep aquifer)
 !      if (shallst(j) > gwqmn(j)) then
-        gw_qdeep(j) = gw_qdeep(j) * alpha_bfe_d(j) + gwseep *           
-     &                                         (1. - alpha_bfe_d(j))
+!        gw_qdeep(j) = gw_qdeep(j) * alpha_bfe_d(j) + gwseep *           
+!     &                                         (1. - alpha_bfe_d(j))
  !     else
  !       gw_qdeep(j) = 0.
  !     end if
